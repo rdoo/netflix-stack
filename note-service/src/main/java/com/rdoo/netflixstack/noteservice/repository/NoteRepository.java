b2a1ced366ac5d4cc80cabfa8d0b4b9faa40792c
rdoo <rafdomaradzki@gmail.com>
2020-02-10 23:19:22 +0100

    remove note service

--- note-service/src/main/java/com/rdoo/netflixstack/noteservice/repository/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rdoo.netflixstack.noteservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.rdoo.netflixstack.noteservice.model.Note;
-
-public interface NoteRepository extends MongoRepository<Note, String> {}
