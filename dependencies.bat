c0d2a40522f6bd03e11b2d3601b69f001c7b1ddf
rdoo <rafdomaradzki@gmail.com>
2020-04-05 23:15:15 +0200

    remove unnecessary files

--- dependencies.bat
+++
@@ -1,5 +0,0 @@
-call mvn dependency:go-offline -f ./discovery-server/pom.xml
-call mvn dependency:go-offline -f ./api-gateway/pom.xml
-call mvn dependency:go-offline -f ./auth-server/pom.xml
-call mvn dependency:go-offline -f ./user-service/pom.xml
-call mvn dependency:go-offline -f ./file-service/pom.xml
--- dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mvn dependency:go-offline -f ./discovery-server/pom.xml
-mvn dependency:go-offline -f ./api-gateway/pom.xml
-mvn dependency:go-offline -f ./auth-server/pom.xml
-mvn dependency:go-offline -f ./user-service/pom.xml
-mvn dependency:go-offline -f ./file-service/pom.xml
